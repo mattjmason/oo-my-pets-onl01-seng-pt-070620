3c4be82bcdbc807ba8bc670da430e7c09fb8a5f9
Matt Mason <masonmj10@gmail.com>
2020-08-05 03:56:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader :species, :name  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def say_species 
+    "I am a #{self.species}."
+  end 
+  
+  def self.count 
+    @@all.count
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+   end 
+  
+  
 end
